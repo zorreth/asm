176371dc8c2564fe581d7334159acdc4b0568a37
Kirill Siukhin <kirillsiukhin@proton.me>
2026-08-24 03:53:27 +0500

    feat: add simple webserver routing

--- webserver-c/main.c
+++ webserver-c/main.c
@@ -2,6 +2,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <string.h>
 
 int main() {
     int sock_fd, conn_fd;
@@ -17,6 +18,9 @@ int main() {
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(8080);
 
+    int opt = 1;
+    setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
     if (bind(sock_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) != 0) {
         perror("socket bind failed");
         return 1;
@@ -29,12 +33,46 @@ int main() {
 
     printf("Server is listening on port 8080...\n");
 
-    socklen_t len = sizeof(client_addr);
+    char buf[1024];
+    socklen_t len;
 
-    conn_fd = accept(sock_fd, (struct sockaddr*)&client_addr, &len);
-    if (conn_fd == -1) {
-        perror("server accept failed");
-        return 1;
+    while (1) {
+        memset(buf, 0, sizeof(buf));
+        len = sizeof(client_addr);
+
+        conn_fd = accept(sock_fd, (struct sockaddr*)&client_addr, &len);
+        if (conn_fd == -1) {
+            perror("server accept failed");
+            return 1;
+        }
+
+        read(conn_fd, buf, sizeof(buf));
+
+        char *path = NULL;
+
+        for (int i = 0; buf[i] != '\0'; i++) {
+            if (buf[i] == ' ') {
+                if (path == NULL) {
+                    path = &buf[i + 1];
+                } else {
+                    buf[i] = '\0';
+                    break;
+                }
+            }
+        }
+
+        char *reply;
+
+        if (strcmp(path, "/") == 0) {
+            reply = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHome Page";
+        } else if (strcmp(path, "/about") == 0) {
+            reply = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nAbout Page";
+        } else {
+            reply = "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\n\r\nNot Found";
+        }
+
+        write(conn_fd, reply, strlen(reply));
+        close(conn_fd);
     }
 
     close(sock_fd);
